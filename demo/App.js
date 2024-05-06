cc2db1d179478f58dc72e3fe431098f3a093a19b
Reid Barber <reid@reidbarber.com>
2024-05-06 14:30:26 -0500

    update demo app

--- demo/App.js
+++ demo/App.js
@@ -1,9 +1,10 @@
 import { unmark, mark } from "../src";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export function App() {
   let [isMarked, setMarked] = useState(false);
   let [elementMap, setElementMap] = useState(null);
+  let demoRef = useRef(null);
 
   useEffect(() => {
     if (!isMarked) {
@@ -18,18 +19,50 @@ export function App() {
       unmark();
       setMarked(false);
     } else {
-      mark({});
+      let elements = mark({});
+      setElementMap(elements);
       setMarked(true);
     }
   };
+  console.log(elementMap);
 
   return (
     <main>
       <h1>WebMarker Demo</h1>
       <button onClick={toggleMark}>{isMarked ? "Unmark" : "Mark"}</button>
+      <div ref={demoRef} style={{ display: "flex", flexDirection: "column" }}>
+        <h2>Interactive Elements:</h2>
+        <div>
+          <label htmlFor="field-1">Input:</label>
+          <input id="field-1" type="text" />
+        </div>
+
+        <div>
+          <label htmlFor="field-2">Input:</label>
+          <input id="field-2" type="text" />
+        </div>
+
+        <div>
+          <label htmlFor="field-3">Input:</label>
+          <input id="field-3" type="text" />
+        </div>
+
+        <div>
+          <button>Button 1</button>
+          <button>Button 2</button>
+        </div>
+      </div>
       <div>
-        <label htmlFor="field-1">Input:</label>
-        <input id="field-1" type="text" />
+        <h2>Marked Elements:</h2>
+        <ul>
+          {elementMap &&
+            Object.entries(elementMap).map(([key, value]) => (
+              <li key={key}>
+                <span>{key}</span>
+                <span>{value}</span>
+              </li>
+            ))}
+        </ul>
       </div>
     </main>
   );
