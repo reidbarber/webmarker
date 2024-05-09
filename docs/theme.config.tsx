00373dc56e6a255b6fdc416e9261fea0bcc79fd8
Reid Barber <reid@reidbarber.com>
2024-05-09 00:38:42 -0500

    add nextra docs

---
+++ docs/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>WebMarker</span>,
+  project: {
+    link: "https://github.com/reidbarber/webmarker",
+  },
+  docsRepositoryBase: "https://github.com/reidbarber/webmarker",
+  footer: {
+    text: "Webmarker",
+  },
+};
+
+export default config;
