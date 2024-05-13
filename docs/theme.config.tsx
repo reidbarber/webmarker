fcdc2a721375ba4538c6573fe1c2c667633cba58
Reid Barber <reid@reidbarber.com>
2024-05-12 20:39:29 -0500

    fix docs title

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
   logo: <span>WebMarker</span>,
+  useNextSeoProps() {
+    const { pathname } = useRouter();
+    if (pathname !== "/") {
+      return {
+        titleTemplate: "%s – WebMarker",
+      };
+    } else {
+      return {
+        title: "WebMarker",
+        description: "Mark web pages for use with vision-language models.",
+      };
+    }
+  },
   project: {
     link: "https://github.com/reidbarber/webmarker",
   },
